cb6283349203b5df8da1d24c8ed1b56a7ddbda73
Giovanna Oliveira <giovannahenrique@edu.unifil.br>
2025-11-26 21:41:41 -0300

    test(frete-validator): adicionar VF06

--- src/tests/FreteValidatorTest.java
+++ src/tests/FreteValidatorTest.java
@@ -47,5 +47,14 @@ import java.util.*;
             assertEquals(1, erros.size());
             assertTrue(erros.contains("Largura acima do limite"));
         }
+
+        @Test
+        void VF06_alturaAcima_retornaErro() {
+            FreteValidator validator = new FreteValidator();
+            Dimensoes d = new Dimensoes(0.50, 0.40, 0.70);
+            List<String> erros = validator.validarPedidoFrete(100.00, 10.0, d, "86020000");
+            assertEquals(1, erros.size());
+            assertTrue(erros.contains("Altura acima do limite"));
+        }
     }
 
