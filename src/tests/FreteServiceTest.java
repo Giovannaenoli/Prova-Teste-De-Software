68909c3e2fafb2cb66a2f4302493700afda44081
Giovanna Oliveira <giovannahenrique@edu.unifil.br>
2025-11-26 21:25:23 -0300

    test(frete): adicionar CF02

--- src/tests/FreteServiceTest.java
+++ src/tests/FreteServiceTest.java
@@ -9,4 +9,11 @@ class FreteServiceTest {
         double frete = service.calcularFrete(300.00, 10.0, "86020000");
         assertEquals(0.00, frete, 0.001);
     }
+
+    @Test
+    void CF02_freteGratis_valorAcima() {
+        FreteService service = new FreteService();
+        double frete = service.calcularFrete(350.00, 25.0, "01001000");
+        assertEquals(0.00, frete, 0.001);
+    }
 }
