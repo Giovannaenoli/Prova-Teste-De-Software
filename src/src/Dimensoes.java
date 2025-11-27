e17a85f6cb676e12df7e4207337773a414c97da6
Giovanna Oliveira <giovannahenrique@edu.unifil.br>
2025-11-26 21:47:00 -0300

    feat(model): adicionar classe Dimensoes

--- src/src/Dimensoes.java
+++ src/src/Dimensoes.java
@@ -5,7 +5,33 @@ public class Dimensoes {
     private double altura;      // em metros
 
     public Dimensoes(double v, double v1, double v2) {
+        this.comprimento = comprimento;
+        this.largura = largura;
+        this.altura = altura;
     }
 
-    // construtor, getters e setters
+    public double getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+        // construtor, getters e setters
+    }
 }
